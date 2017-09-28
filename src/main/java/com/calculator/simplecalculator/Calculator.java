ac4d7db7e8c3e10377744d92b4d674db11e1f3a1
“mkosecki1 <mkosecki@op.pl>
2017-09-28 22:25:25 +0200

    Calculator 4.6.

---
+++ src/main/java/com/calculator/simplecalculator/Calculator.java
@@ -0,0 +1,35 @@
+package com.calculator.simplecalculator;
+
+public class Calculator {
+
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int substractAFromB(int a, int b) {
+        return a - b;
+    }
+
+    public void someMethod(){
+        System.out.println("It works!");
+    }
+}
+
+class Application
+{
+    public static void main(String[] args) {
+
+        Calculator simpleCalculator1 = new Calculator();
+        int result1 = simpleCalculator1.addAToB(7, 12);
+        System.out.println("Result A + B is: " + result1);
+
+        Calculator simpleCalculator2 = new Calculator();
+        int result2 = simpleCalculator2.substractAFromB(11, 4);
+        System.out.println("Result A - B is: " + result2);
+
+        Calculator simpleCalculator3 = new Calculator();
+        simpleCalculator3.someMethod();
+
+    }
+
+}
